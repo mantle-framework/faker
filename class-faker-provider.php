20e925cf13ce03cfd216654c82d3a58cd6f04504
mantle-ci <mantle@alley.com>
2024-05-08 19:42:50 +0000

    Fix the method being called for Command_Event

--- class-faker-provider.php
+++ class-faker-provider.php
@@ -18,7 +18,6 @@ class Faker_Provider extends Base {
 	 * Compile a set of blocks.
 	 *
 	 * @param array $blocks Blocks to compile.
-	 * @return string
 	 */
 	public static function blocks( array $blocks ): string {
 		return implode( "\n\n", $blocks );
@@ -28,7 +27,6 @@ class Faker_Provider extends Base {
 	 * Build a heading block.
 	 *
 	 * @param int $level Heading level.
-	 * @return string
 	 */
 	public static function heading_block( int $level = 2 ): string {
 		return static::block(
@@ -44,7 +42,6 @@ class Faker_Provider extends Base {
 	 * Build a paragraph block.
 	 *
 	 * @param int $sentences Number of sentences in the block.
-	 * @return string
 	 */
 	public static function paragraph_block( int $sentences = 3 ): string {
 		return static::block(
@@ -70,12 +67,28 @@ class Faker_Provider extends Base {
 	}
 
 	/**
+	 * Build an image block.
+	 *
+	 * @param string|null $url Image URL.
+	 * @param string|null $alt Image alt text.
+	 * @param array       $attributes Additional attributes for the block.
+	 */
+	public function image_block( ?string $url = null, ?string $alt = null, array $attributes = [] ) {
+		$image = sprintf(
+			'<figure class="wp-block-image"><img src="%s"%s/></figure>',
+			$url ?? 'https://picsum.photos/' . wp_rand( 100, 1000 ) . '/' . wp_rand( 100, 1000 ),
+			$alt ? ' alt="' . esc_attr( $alt ) . '"' : '',
+		);
+
+		return static::block( 'image', $image, $attributes );
+	}
+
+	/**
 	 * Build a block for Gutenberg.
 	 *
 	 * @param string $block_name Block name.
 	 * @param string $content Content for the block.
 	 * @param array  $attributes Attributes for the block.
-	 * @return string
 	 */
 	public static function block( string $block_name, string $content = '', array $attributes = [] ): string {
 		// Add a newline before and after the content.
